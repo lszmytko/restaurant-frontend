5b5d8bae887545185e7cfb6945a6f2cbc2a38ba9
Łukasz Szmytko <lukasz.szmytko@grupawp.pl>
2023-01-03 23:38:12 +0100

    refactor(refactor): refactor several smaller components

---
+++ src/components/LogOut/LogOut.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { useGlobalContext } from "../../context/context";
+
+const LogOut = () => {
+  const { setUserInfo } = useGlobalContext();
+
+  const handleLogOut = () => {
+    setUserInfo({
+      userId: "",
+      isLogged: false,
+      name: "",
+      email: "",
+      password: "",
+      flatnumber: "",
+      phone: ""
+    });
+
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+  };
+
+  return (
+    <aside className="LogOut">
+      <Link to="/" className="LogOut" onClick={handleLogOut}>
+        Wyloguj
+      </Link>
+    </aside>
+  );
+};
+
+export default LogOut;
