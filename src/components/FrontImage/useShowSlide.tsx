d8fb29c5e6e912caee85aad4094789ba143dd03c
Łukasz Szmytko <lukasz.szmytko@grupawp.pl>
2023-01-03 23:12:04 +0100

    refactor(refactor): refactor FrontImage component

--- src/components/FrontImage/useShowSlide.tsx
+++ src/components/FrontImage/useShowSlide.tsx
@@ -1,1 +1,25 @@
-export const useShowSlide = () => {};
+import { useEffect, useState } from "react";
+
+import salad from "../../images/frontSlider/salad.jpg";
+import bolognese from "../../images/frontSlider/bolognese.jpg";
+import pierogi from "../../images/frontSlider/pierogi.jpg";
+
+const slides = [salad, bolognese, pierogi];
+
+export const useShowSlide = () => {
+  const [activeSlide, setActiveSlide] = useState(0);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setActiveSlide((prevSlide) => {
+        if (prevSlide === slides.length - 1) {
+          return 0;
+        }
+        return prevSlide + 1;
+      });
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [activeSlide]);
+
+  return { slides, setActiveSlide, activeSlide };
+};
